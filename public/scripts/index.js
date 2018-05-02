514a6bbf8877a6095f1583f5cbf13e214de045f4
patersog <gianluca.paterson@gmail.com>
2018-05-02 14:35:04 -0400

    Update dependencies for test; Add basic POST and GET tests for /api/notes endpoint

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,24 +1,24 @@
 /* global $ noteful api store */
-'use strict';
+
 
 $(document).ready(function () {
-  noteful.bindEventListeners();
+	noteful.bindEventListeners();
 
-  Promise.all([
-    api.search('/api/notes'),
-    api.search('/api/folders'),
-    api.search('/api/tags')
-  ])
-    .then(([
-      notes,
-      folders,
-      tags
-    ]) => {
-      store.notes = notes;
-      store.folders = folders;
-      store.tags = tags;
-      noteful.render();
-    });
+	Promise.all([
+		api.search('/api/notes'),
+		// api.search('/api/folders'),
+		// api.search('/api/tags')
+	])
+		.then(([
+			notes,
+			folders,
+			tags
+		]) => {
+			store.notes = notes;
+			store.folders = folders;
+			store.tags = tags;
+			noteful.render();
+		});
 
 });
 
